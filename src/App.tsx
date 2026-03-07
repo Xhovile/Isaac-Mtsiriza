d558f6f7befd907cd9a270c3e5571bfa91d47072
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-07 23:21:44 +0200

    Remove listing URL parameter management functions
    Removed functions related to listing URL parameters.

--- src/App.tsx
+++ src/App.tsx
@@ -57,28 +57,6 @@ import ReportProblemPage from "./components/ReportProblemPage";
 import ChangePasswordModal from "./components/ChangePasswordModal";
 import ReportListingModal from "./components/ReportListingModal";
 
-const getListingIdFromUrl = () => {
-  const params = new URLSearchParams(window.location.search);
-  return params.get("listing");
-};
-
-const buildListingShareUrl = (id: string | number) => {
-  const url = new URL(window.location.href);
-  url.searchParams.set("listing", String(id));
-  return url.toString();
-};
-
-const syncListingParamInUrl = (id: string | number) => {
-  const url = new URL(window.location.href);
-  url.searchParams.set("listing", String(id));
-  window.history.replaceState({}, "", url.toString());
-};
-
-const clearListingParamFromUrl = () => {
-  const url = new URL(window.location.href);
-  url.searchParams.delete("listing");
-  window.history.replaceState({}, "", url.toString());
-};
 
 // --- Components ---
  const Navbar = ({ 
